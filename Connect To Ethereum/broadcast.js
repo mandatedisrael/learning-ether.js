17fca8f0d4b208a8720822e6acd04c91bc67bc43
Dami King <mandatedisrael@gmail.com>
2022-07-24 09:46:29 +0100

    Create broadcast.js

---
+++ Connect To Ethereum/broadcast.js
@@ -0,0 +1,21 @@
+const { Wallet, utils, providers } = require('ethers');
+const { ganacheProvider, PRIVATE_KEY } = require('./config');
+
+//create a new web3 provider with ganache
+const provider = new providers.Web3Provider(ganacheProvider); 
+
+//create a new wallet
+const wallet = new Wallet(PRIVATE_KEY);
+
+async function sendEther({ value, to }) {
+    const rawTx = await wallet.signTransaction({ 
+        value, to, 
+        gasLimit: 0x5208,
+        gasPrice: 0x3b9aca00 
+    });
+
+    //send the transaction and return the transaction promise
+    return provider.sendTransaction(rawTx);
+}
+
+module.exports = sendEther;
