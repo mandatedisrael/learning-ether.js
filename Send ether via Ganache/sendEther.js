7d1fe0d166c539999e59dd7d91d196db2e4781d5
mandatedisrael <mandatedisrael@gmail.com>
2022-07-26 00:47:23 +0100

    update Send Ether via Ganache

--- Send ether via Ganache/sendEther.js
+++ Send ether via Ganache/sendEther.js
@@ -9,4 +9,5 @@ async function main(){
     const privateKey = "7712e6ad4235614de2b09ec92279ea5810e8e322309c3bf4f9b3e107f3d96449"; //no security issues, just a ganache localaccount
     const wallet = new ethers.Wallet(privateKey, provider);
     
+    const signer1 = provider.getSigner(1);
 }
