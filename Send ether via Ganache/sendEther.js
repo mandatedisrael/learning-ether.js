44cd16bd5a78618cc67550abc54ea8f2637b9197
mandatedisrael <mandatedisrael@gmail.com>
2022-07-26 15:48:39 +0100

    Create a .env file and update the .gitignore file

--- Send ether via Ganache/sendEther.js
+++ Send ether via Ganache/sendEther.js
@@ -1,13 +1,35 @@
 const ethers = require('ethers');
 
-async function main(){
+async function main() {
     //Ganache rpc 
     const url = "HTTP://127.0.0.1:7545";
   
     const provider = new ethers.providers.JsonRpcProvider(url);
   
-    const privateKey = "7712e6ad4235614de2b09ec92279ea5810e8e322309c3bf4f9b3e107f3d96449"; //no security issues, just a ganache localaccount
+    const privateKey = "16bb9b14c80d35ba7b8bf0909e6071aaf23ff68fdf8308ff01bf969b1da9834e"; //no security issues, just a ganache localaccount
     const wallet = new ethers.Wallet(privateKey, provider);
     
     const signer1 = provider.getSigner(1);
+    const addr1 = await signer1.getAddress();
+    const walletBalance = await wallet.getBalance();
+
+    console.log("Balance of sender address before txn: "+ ethers.utils.formatEther(walletBalance));
+    console.log("Sending ether from "+ wallet.address+ " to "+ addr1);
+
+    const txn =  await wallet.sendTransaction({
+        to: addr1,
+        value: ethers.utils.parseEther("50"),
+    });
+
+    const receipt  = await txn.wait();
+    const balanceAfter = await wallet.getBalance();
+    console.log("Balance of sender address after the txn: "+ ethers.utils.formatEther(balanceAfter)); //parseEther for BigNumber rep while formatEther for string rep
+
 }
+
+main()
+.then(() => process.exit(0))
+.catch(error => {
+    console.log(error);
+    process.exit(1);
+});
